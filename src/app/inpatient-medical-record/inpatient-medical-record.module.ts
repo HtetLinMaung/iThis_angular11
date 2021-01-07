72ef30d0be5bac4312ccf02508a01a79ceb7130c
Htet Lin Maung <hlm@mit.com.mm>
2021-01-07 07:27:45 +0630

    'update'

--- src/app/inpatient-medical-record/inpatient-medical-record.module.ts
+++ src/app/inpatient-medical-record/inpatient-medical-record.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { InstructionComponent } from './instruction/instruction.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { InstructionFormComponent } from './instruction/instruction-form/instruction-form.component';
+import { InstructionListComponent } from './instruction/instruction-list/instruction-list.component';
 
 @NgModule({
-  declarations: [InstructionComponent],
+  declarations: [InstructionComponent, InstructionFormComponent, InstructionListComponent],
   imports: [
     CommonModule,
     FormsModule,
