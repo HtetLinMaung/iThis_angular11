eaedbd38fe190ac5309b5b903b71c3d41e672420
Htet Lin Maung <hlm@mit.com.mm>
2021-01-08 13:14:31 +0630

    'update'

--- src/app/inpatient-medical-record/instruction/instruction-form/instruction-form.component.ts
+++ src/app/inpatient-medical-record/instruction/instruction-form/instruction-form.component.ts
@@ -27,6 +27,9 @@ export class InstructionFormComponent implements OnInit {
 
   ngOnInit(): void {
     this.bindEditData();
+    this.filteredInstructions = this.instructionStoreService.instructions.filter(
+      (instruction) => instruction.pId == this.appStoreService.pId
+    );
   }
 
   new() {
@@ -93,28 +96,93 @@ export class InstructionFormComponent implements OnInit {
 
   print() {
     const doc = new jsPDF();
-    // doc.text('Hello world!', 10, 10);
-    this.filteredInstructions = this.instructionStoreService.instructions.filter(
-      (instruction) => instruction.pId == this.appStoreService.pId
-    );
+
+    (doc as any).autoTable({
+      html: '#inpatient__record',
+      startY: 35,
+      theme: 'grid',
+      didDrawCell: (data) => {
+        console.log(data.row);
+        switch (data.row.index) {
+          case 1:
+            doc.setFontSize(9);
+            switch (data.column.index) {
+              case 0:
+                doc.text('WARD', data.cell.x + 2, data.cell.y + 5);
+                break;
+              case 1:
+                doc.text('BED', data.cell.x + 2, data.cell.y + 5);
+                break;
+            }
+            break;
+          case 2:
+            doc.setFontSize(9);
+            doc.text('DRUG ALLERGY', data.cell.x + 1, data.cell.y + 3);
+            doc.text('DRUG ALLERGY TO:', data.cell.x + 40, data.cell.y + 3);
+
+            doc.rect(data.cell.x + 3, data.cell.y + 5, 5, 5);
+            doc.text('YES', data.cell.x + 10, data.cell.y + 9);
+
+            doc.rect(data.cell.x + 25, data.cell.y + 5, 5, 5);
+            doc.text('NO', data.cell.x + 32, data.cell.y + 9);
+            break;
+        }
+      },
+      // willDrawCell: (data) => {
+      //   switch (data.row.index) {
+      //     case 0:
+      //       break;
+      //   }
+      // },
+      styles: {
+        minCellWidth: 15,
+        fontSize: 11,
+        cellPadding: 4,
+        valign: 'middle',
+        halign: 'center',
+      },
+    });
+
+    doc.setFontSize(11);
+    doc.text('ASIA ROYAL HOSPITAL', 105, 15, { align: 'center' });
+    doc.text('IN-PATIENT MEDICATION RECORD - INSTRUCTION', 105, 23, {
+      align: 'center',
+    });
+
+    const y = 80;
+
+    doc.setFontSize(10);
+    doc.text('HT: ', 18, y);
+    doc.line(25, y, 45, y);
+
+    doc.text('WT: ', 55, y);
+    doc.line(62, y, 82, y);
+
+    doc.text('DATE TAKEN: ', 135, y);
+    doc.line(147 + 15, y, 180 + 15, y);
+
+    doc.setFontSize(9);
+    doc.text('(To be taken on admission)', 31, y + 3);
+
     (doc as any).autoTable({
       html: '#instruction__print',
-      startY: 60,
+      startY: 90,
       theme: 'grid',
       willDrawCell: (data) => {
         switch (data.row.index) {
           case 0:
-            doc.setFillColor('#CDC8C8');
-            doc.setTextColor('#fff');
+            // doc.setFillColor('#CDC8C8');
+            // doc.setTextColor('#fff');
             break;
         }
       },
       styles: {
-        fontSize: 9,
-        minCellHeight: 1,
-        cellPadding: 1,
+        fontSize: 12,
         valign: 'middle',
         halign: 'center',
+      },
+      headStyles: {
+        fillColor: '#686869',
       },
     });
     doc.save('a4.pdf');
