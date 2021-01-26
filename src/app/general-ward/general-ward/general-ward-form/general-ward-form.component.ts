e58fd299339654c029e3be77212f665a18de3dff
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro.local>
2021-01-26 14:54:32 +0630

    update

--- src/app/general-ward/general-ward/general-ward-form/general-ward-form.component.ts
+++ src/app/general-ward/general-ward/general-ward-form/general-ward-form.component.ts
@@ -10,6 +10,7 @@ export class GeneralWardFormComponent implements OnInit {
 
   formData = [
     {
+      icon: 'fa-lungs',
       problemName: 'Breathing',
       items: [
         'O2 Therapy- Nasal/ Facemask/ Trachy Mask/ Ventilator ',
@@ -18,12 +19,75 @@ export class GeneralWardFormComponent implements OnInit {
         'Oral/ Laryngeal Suctioning',
       ].map((v) => ({
         interventions: v.split('/'),
+        selectedInterventions: '0',
         initialDate: '',
         dayNurse: false,
         nightNurse: false,
         dayNurseAt: '',
         nightNurseAt: '',
         outcomeMet: 'y',
+        readonly: true,
+      })),
+    },
+    {
+      icon: 'fa-heartbeat',
+      problemName: 'Circulation',
+      items: [
+        'Circulation',
+        'Monitor Blood Pressure',
+        'Perform Neurovascular Assessment',
+        'Assess for Bleeding',
+        'Assess for Redness/ Swelling of Limbs',
+        'Observe Diabeties Right foot (last digit)',
+      ].map((v) => ({
+        interventions: v.split('/'),
+        selectedInterventions: '0',
+        initialDate: '',
+        dayNurse: false,
+        nightNurse: false,
+        dayNurseAt: '',
+        nightNurseAt: '',
+        outcomeMet: 'y',
+        readonly: true,
+      })),
+    },
+    {
+      icon: 'fa-brain',
+      problemName: 'Communication',
+      items: [
+        'Communication',
+        'Monitor Conscious Level/ Mental Status',
+        'Provide Atternative Means of Communication',
+        'Update Patient/ Family on Progress',
+      ].map((v) => ({
+        interventions: v.split('/'),
+        selectedInterventions: '0',
+        initialDate: '',
+        dayNurse: false,
+        nightNurse: false,
+        dayNurseAt: '',
+        nightNurseAt: '',
+        outcomeMet: 'y',
+        readonly: true,
+      })),
+    },
+    {
+      icon: 'fa-smile',
+      problemName: 'Comfort',
+      items: [
+        'Comfort',
+        'Pharmacological Pain Relief Provided',
+        'Non-Pharmacological Pain Relief Provided',
+      ].map((v) => ({
+        interventions: v.split('/'),
+        selectedInterventions: '0',
+        initialDate: '',
+        dayNurse: false,
+        nightNurse: false,
+        dayNurseAt: '',
+        nightNurseAt: '',
+        outcomeMet: 'y',
+        readonly: true,
       })),
     },
   ];
@@ -33,5 +97,19 @@ export class GeneralWardFormComponent implements OnInit {
     const tabEle2 = document.getElementById('tab2');
     tabEle2.style.background = '#3b5998';
     tabEle1.style.background = '#8C9899';
+  }
+
+  addRow(items) {
+    items.push({
+      interventions: [''],
+      selectedInterventions: '0',
+      initialDate: '',
+      dayNurse: false,
+      nightNurse: false,
+      dayNurseAt: '',
+      nightNurseAt: '',
+      outcomeMet: 'y',
+      readonly: false,
+    });
   }
 }
