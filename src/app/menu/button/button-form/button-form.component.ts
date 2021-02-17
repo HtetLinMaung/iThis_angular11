69f4cdd19468e78838019f9328d6eebeeec4067c
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro.local>
2021-02-17 14:41:56 +0630

    update

--- src/app/menu/button/button-form/button-form.component.ts
+++ src/app/menu/button/button-form/button-form.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ButtonStoreService } from '../button-store.service';
 
 @Component({
   selector: 'app-button-form',
   templateUrl: './button-form.component.html',
-  styleUrls: ['./button-form.component.css']
+  styleUrls: ['./button-form.component.css'],
 })
 export class ButtonFormComponent implements OnInit {
+  code = '';
+  description = '';
 
-  constructor() { }
+  constructor(public buttonStoreService: ButtonStoreService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  new() {}
+
+  save() {}
+
+  delete() {}
 }
