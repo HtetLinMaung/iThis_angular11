61f7c64e8e57dab807f4ab21e9c08241700d796b
hein335 <57986083+hein335@users.noreply.github.com>
2021-02-24 09:27:08 +0630

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,10 @@ import { LoginComponent } from './login/login.component';
 import { NurseShiftSummaryModule } from './nurse-shift-summary/nurse-shift-summary.module';
 import { NursingActivityWorklistModule } from './nursing-activity-worklist/nursing-activity-worklist.module';
 import { NursingCareRecordModule } from './nursing-care-record/nursing-care-record.module';
+import { PatientTypeModule } from './patient-type/patient-type.module';
+import { RoomcodeModule } from './roomcode/roomcode.module';
+import { RoomrateModule } from './roomrate/roomrate.module';
+import { RouteModule } from './route/route.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
@@ -26,7 +30,11 @@ import { SharedModule } from './shared/shared.module';
     SharedModule,
     NursingCareRecordModule,
     CommonTypeModule,
+    RouteModule,
     GeneralWardModule,
+    PatientTypeModule,
+    RoomrateModule,
+    RoomcodeModule,
     RouterModule.forRoot([
       {
         path: '',
